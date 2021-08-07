e0296562c1cc9c93e605d823710864c99c28f672
Nikhar <pandyanikhar@gmail.com>
2021-08-07 15:07:26 -0400

    server update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
-require("dotenv").config();
+require("dotenv").config({ encoding: "latin1" });
 
 const app = express();
 
